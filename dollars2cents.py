4dadcabef2099c8d2d9e104c5b84f7e96dcd2366
incogyeeto <scdefevers@gmail.com>
2025-05-10 00:38:39 -0500

    My solution for florinpop17's dollars2cents This practice problem came from florinpop17 / app-ideas github repo.

---
+++ dollars2cents.py
@@ -0,0 +1,42 @@
+def main():
+
+    def changeConverter(dollars):
+        dollars = dollars.replace('$', '') # Removes the '$' for changing type to int
+
+        ''' The try/except/else is used here in the case the user inputs a whole number instead of a float, there would only be one value. The else statment catches cases if they input something less than 1. '''
+        try: 
+            dollars, cents = dollars.split('.') # Splits the dollars and cents apart
+        except ValueError:
+            cents = 100 * int(dollars)
+        else:
+            if dollars != '':
+                cents = int(cents) + (int(dollars) * 100) # Combines into the change value of both dollars and cents
+        return int(cents)
+
+    def coinDistributor(change):
+        quarters = 0 # Initialize a count for all coin types
+        dimes = 0
+        nickels = 0
+        pennies = 0
+        ''' A loop to check through all the coin types. Checks for all potential quarters first then goes through the other coins. Modulo operator allows for it to check through and if it isn't 0 it subtracts a penny. If it is a number like 0.07 it would remove the pennies first then the nickel. '''
+        while change > 0:
+            if change / 25 >= 1:
+                change -= 25
+                quarters += 1
+            elif change % 10 == 0:
+                change -= 10
+                dimes += 1
+            elif change % 5 == 0:
+                change -= 5
+                nickels += 1
+            else:
+                change -= 1
+                pennies += 1
+            
+        print(f'Quarters:{quarters} \nDimes:{dimes} \nNickels:{nickels} \nPennies:{pennies}') # Prints each coin type and their count
+
+    money = input('Dollars: ')
+    money = changeConverter(money)
+    coinDistributor(money)
+
+main()
